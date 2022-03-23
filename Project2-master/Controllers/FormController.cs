6c07ee070b95fde3fcf32c7b79287feae1791cfe
ShengLei123 <starkray666666@gmail.com>
2022-03-23 11:05:28 -0600

    update

---
+++ Project2-master/Controllers/FormController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project2.Models;
+
+namespace Project2.Controllers
+{
+    public class FormController : Controller
+    {
+        [HttpGet]
+        public IActionResult Form()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Form(Schedules s)
+        {
+            return View(s);
+        }
+
+
+    }
+}
---
+++ Project2-master/Models/Schedules.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project2.Models
+{
+    public class Schedules
+    {
+        [Key]
+        [Required]
+        public int ScheduleId { get; set; }
+        public string GroupName { get; set; }
+        public int GroupSize { get; set; }
+        public string GroupEmail { get; set; }
+        public string GroupPhone { get; set; }
+        public string StartDate { get; set; }
+        public string StartTime { get; set; }
+    }
+}
